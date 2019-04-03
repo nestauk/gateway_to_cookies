e153fcc86eeded6c2d9115b4984c2705b16875d1
bishax <alex.bishop@nesta.org.uk>
2019-04-03 12:56:29 +0100

    Initial commit of src for analysis

---
+++ gateway_to_cookies/visualization/data/gtr.py
@@ -0,0 +1,61 @@
+from pandas import read_csv
+from src.features.text_preprocessing import tokenize_document
+
+
+def make_gtr(data_dir, nrows=None):
+    """Clean and tokenise gateway to research abstract texts
+
+
+    Args:
+        data_dir (str): data directory
+        nrows (int, optional): number of rows to use, defaults to None.
+
+    """
+
+    (read_csv(f"{data_dir}/raw/gtr_projects.csv", nrows=nrows)
+     .pipe(clean_gtr)
+     .pipe(transform_gtr)
+     .to_csv(f"{data_dir}/processed/gtr_tokenised.csv")
+     )
+
+
+def clean_gtr(gtr_df):
+    """Remove duplicate project_id's and missing values
+
+    Args:
+        gtr_df (pandas.DataFrame): Gateway to research data
+
+    Returns:
+        pandas.DataFrame
+    """
+
+    return (gtr_df
+            .drop_duplicates('project_id')
+            .dropna()
+            )
+
+
+def transform_gtr(gtr_df):
+    """Tokenise Gateway to Research abstract texts
+
+    Args:
+        gtr_df (pandas.DataFrame): Gateway to research data
+
+    Returns:
+        pandas.DataFrame
+
+        Tokenised dataset
+    """
+
+    processed = (gtr_df.abstract_texts
+                 .apply(tokenize_document, flatten=True)
+                 .to_frame('processed_documents')
+                 # Keep only documents with tokenised terms:
+                 .assign(is_doc=lambda x: x.processed_documents.apply(len) > 0)
+                 .query("is_doc > 0")
+                 )
+
+    return (gtr_df
+            .join(processed)
+            .dropna()
+            )
--- gateway_to_cookies/visualization/data/make_dataset.py
+++ gateway_to_cookies/visualization/data/make_dataset.py
@@ -3,34 +3,28 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-from src.data.gtr import make_gtr
 
 
 @click.command()
-@click.option('--nrows', '-n', default=None, type=int)
-def main(nrows):
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
+def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
-
-    Args:
-        nrows (int, optional): Number of rows of dataset to make. Defaults to None.
     """
     logger = logging.getLogger(__name__)
-
-    logger.info(f'making gateway to research data set from raw data ({nrows} rows)')
-    make_gtr(project_dir / 'data', nrows)
+    logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # Define project base directory
+    # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    # NOTE: not used in this stub but often useful
     load_dotenv(find_dotenv())
 
     main()

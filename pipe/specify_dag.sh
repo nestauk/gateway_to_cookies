0cb9b213ef2274a439c667ab84ad0a5b5a8dc667
bishax <alex.bishop@nesta.org.uk>
2019-04-04 10:15:35 +0100

    Updated `specify_dag.sh` to track outputs with git

--- pipe/specify_dag.sh
+++ pipe/specify_dag.sh
@@ -10,6 +10,8 @@ dvc run -w ..\
   -o data/processed/gtr_tokenised.csv\
   python gateway_to_cookies/data/make_dataset.py -n 10
 
+git add ../data/processed/.gitignore gtr_tokenised.csv.dvc
+
 # Train word embeddings
 dvc run -w .. \
   -d data/processed/gtr_tokenised.csv\
@@ -18,6 +20,8 @@ dvc run -w .. \
   -o models/gtr_w2v\
   -o data/processed/gtr_embedding.csv\
   python gateway_to_cookies/features/build_features.py
+
+git add ../models/.gitignore ../data/processed/.gitignore gtr_w2v.dvc
 
 # Test-train split
 dvc run -w ..\
@@ -28,12 +32,17 @@ dvc run -w ..\
   -o data/processed/gtr_test.csv\
   python gateway_to_cookies/models/train_test_split.py
 
+git add ../data/processed/.gitignore gtr_train.csv.dvc
+
+
 # Train model
 dvc run -w ..\
   -d data/processed/gtr_train.csv\
   -d gateway_to_cookies/models/train_model.py\
   -o models/gtr_forest.pkl\
   python gateway_to_cookies/models/train_model.py
+
+git add ../models/.gitignore gtr_forest.pkl.dvc
 
 # Evaluate model
 touch ../models/metrics.txt
@@ -44,3 +53,5 @@ dvc run -w ..\
   -M models/metrics.txt\
   -f Dvcfile\
   python gateway_to_cookies/models/evaluate.py
+
+git add Dvcfile ../models/metrics.txt

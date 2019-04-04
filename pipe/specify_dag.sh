647f3b86d73220e6fcd7a3c194e26e141030493d
bishax <alex.bishop@nesta.org.uk>
2019-04-04 09:13:19 +0100

    Fixed dag specification to have dependencies on the command called

--- pipe/specify_dag.sh
+++ pipe/specify_dag.sh
@@ -6,6 +6,7 @@ set -e
 dvc run -w ..\
   -d data/raw/gtr_projects.csv\
   -d gateway_to_cookies/features/text_preprocessing.py\
+  -d gateway_to_cookies/data/make_dataset.py\
   -o data/processed/gtr_tokenised.csv\
   python gateway_to_cookies/data/make_dataset.py -n 10
 
@@ -13,6 +14,7 @@ dvc run -w ..\
 dvc run -w .. \
   -d data/processed/gtr_tokenised.csv\
   -d gateway_to_cookies/features/w2v.py\
+  -d gateway_to_cookies/features/build_features.py\
   -o models/gtr_w2v\
   -o data/processed/gtr_embedding.csv\
   python gateway_to_cookies/features/build_features.py
@@ -21,6 +23,7 @@ dvc run -w .. \
 dvc run -w ..\
   -d data/processed/gtr_embedding.csv\
   -d data/processed/gtr_tokenised.csv\
+  -d gateway_to_cookies/models/train_test_split.py\
   -o data/processed/gtr_train.csv\
   -o data/processed/gtr_test.csv\
   python gateway_to_cookies/models/train_test_split.py
@@ -28,6 +31,7 @@ dvc run -w ..\
 # Train model
 dvc run -w ..\
   -d data/processed/gtr_train.csv\
+  -d gateway_to_cookies/models/predict_model.py\
   -o models/gtr_forest.pkl\
   python gateway_to_cookies/models/predict_model.py
 
@@ -36,6 +40,7 @@ touch ../models/metrics.txt
 dvc run -w ..\
   -d data/processed/gtr_test.csv\
   -d models/gtr_forest.pkl\
+  -d gateway_to_cookies/models/evaluate.py\
   -M models/metrics.txt\
   -f Dvcfile\
   python gateway_to_cookies/models/evaluate.py
